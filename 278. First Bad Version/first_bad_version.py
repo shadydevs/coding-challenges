052212ccdfc66ee40a388a66ae2c4f2a0f582513
Shady Nessim <shady53@aucegypt.edu>
2022-02-01 21:19:54 +0200

    solution 1 O(n)

---
+++ 278. First Bad Version/first_bad_version.py
@@ -0,0 +1,8 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        while isBadVersion(n):
+            n-=1
+        return n + 1
