78e777be7bf7565df29375173db75b22811cf5c2
Shady Nessim <shady53@aucegypt.edu>
2022-02-01 21:57:41 +0200

    final solution O(log n)

--- 278. First Bad Version/first_bad_version.py
+++ 278. First Bad Version/first_bad_version.py
@@ -3,6 +3,27 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        while isBadVersion(n):
-            n-=1
-        return n + 1
+        """finds first n where isBadVersion returns true for
+
+        Args:
+            n (int): number of versions
+
+        Returns:
+            int: first n
+        """        
+        i: int = 1
+        j: int = n
+        
+        first: int = isBadVersion(i)
+        last: int = isBadVersion(j)
+            
+        if not isBadVersion(n-1):
+            return n
+        
+        while i <= j:
+            k: int = int((i+j)/2)
+            if not isBadVersion(k):
+                i = k + 1
+            elif isBadVersion(k):
+                j = k - 1
+        return i
