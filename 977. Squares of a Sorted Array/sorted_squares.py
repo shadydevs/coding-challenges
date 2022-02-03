996b778b180aabbd7f41741023b4723b472f9ada
Shady Nessim <shady53@aucegypt.edu>
2022-02-04 01:54:43 +0200

    final solution (simple, improved O(n))

--- 977. Squares of a Sorted Array/sorted_squares.py
+++ 977. Squares of a Sorted Array/sorted_squares.py
@@ -1,30 +1,5 @@
-def searchInsert(nums: list[int], target: int) -> int:
-    
-    i: int = 0
-    j: int = len(nums) - 1
-
-    while i <= j:
-        k: int = int((i+j)/2)
-        if target > nums[k]:
-            i = k + 1
-        elif target < nums[k]:
-            j = k - 1
-        else:
-            return k
-    return i
-
-def sortedSquares(nums: list[int]) -> list[int]:
-#         sorted()
-    neg = []
-    pos = []
+def sortedSquares(self, nums: List[int]) -> List[int]:
+    newNums = []
     for n in nums:
-        if n < 0:
-            neg.append(n*n)
-        else:
-            pos.append(n*n)
-
-    for n in neg:
-        pos.insert(searchInsert(pos, n), n)
-    return pos
-
-print(sortedSquares([-4,-1,0,3,10]))
+        newNums.append(n*n)
+    return sorted(newNums)
